a6bef90e2a331cb7f444b644065c022c701752cf
ZBr14n <lamb2@wit.edu>
2019-07-22 09:19:08 -0400

    add db url and name

--- client/src/App.js
+++ client/src/App.js
@@ -1,64 +1,141 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Table } from 'reactstrap';
+
 
 class App extends Component {
   // Initialize state
-  state = { passwords: [] }
+  state = { 
+    passwords: [],
+    emails: []
+  }
 
-  // Fetch passwords after first mount
+  // Fetch passwords and emails after first mount
   componentDidMount() {
-    this.getPasswords();
+    // this.getPasswords();
+    this.getEmails_Passwords();
   }
 
-  getPasswords = () => {
-    // Get the passwords and store them in state
+
+  getEmails_Passwords=()=>{
+    fetch('/api/emails')
+      .then(res=>res.json())
+      .then(emails=>this.setState({emails}));
+
+
     fetch('/api/passwords')
-      .then(res => res.json())
-      .then(passwords => this.setState({ passwords }));
+    .then(res => res.json())
+    .then(passwords => this.setState({ passwords }));
   }
+
+
+
+  upload_data=()=>{
+    fetch('/api/upload',{method:'POST'})
+      .then(response => {
+        if(response.ok){
+          console.log("click recorded");
+          return;
+        }
+        throw new Error("Request failed.");
+      })
+      .catch(function(error){
+        console.log(error);
+      });
+  }
+
+
+
 
   render() {
     const { passwords } = this.state;
+    const { emails } = this.state;
+
+    const style1={
+      padding: '10%',
+      width: '30%',
+      float: 'left'
+    };
+
+
+
 
     return (
       <div className="App">
-        {/* Render the passwords if we have them */}
-        {passwords.length ? (
           <div>
-            <h1>5 Passwords.</h1>
-            <ul className="passwords">
-              {/*
-                Generally it's bad to use "index" as a key.
-                It's ok for this example because there will always
-                be the same number of passwords, and they never
-                change positions in the array.
-              */}
-              {passwords.map((password, index) =>
-                <li key={index}>
-                  {password}
-                </li>
-              )}
+
+          <ul style={style1}>
+            {emails.map((email, index) =>
+            <tr key={index}>
+              {email}
+            </tr>
+            )}
             </ul>
+
+
+            
+            <ul style={style1}>
+            {passwords.map((password, index) =>
+            <tr key={index}>
+              {password}
+            </tr>
+            )}
+            </ul>
+
+            {/* <ul style={style1}>
+            {emails.map((email, index) =>
+            <li key={index}>
+              {email}
+            </li>
+            )}
+            </ul>
+
+
+            
+            <ul style={style1}>
+            {passwords.map((password, index) =>
+            <li key={index}>
+              {password}
+            </li>
+            )}
+            </ul> */}
+          </div>
+   
+
+
+          <div>
             <button
               className="more"
-              onClick={this.getPasswords}>
+              onClick={this.getEmails_Passwords}>
               Get More
             </button>
+
+            <div>
+              <br />
+              <button className="upload" onClick={this.upload_data}> Upload Data </button>
+            </div>
           </div>
-        ) : (
-          // Render a helpful message otherwise
-          <div>
-            <h1>No passwords :(</h1>
-            <button
-              className="more"
-              onClick={this.getPasswords}>
-              Try Again?
-            </button>
-          </div>
-        )}
+
+
+          
+        
+        
+
       </div>
     );
   }
 }
 
 export default App;
+
+
+
+
+
+  //The below code has been merged with getEmails_Passwords()
+  // getPasswords = () => {
+  //   // Get the passwords and store them in state
+  //   fetch('/api/passwords')
+  //     .then(res => res.json())
+  //     .then(passwords => this.setState({ passwords }));
+  // }
--- index.js
+++ index.js
@@ -1,11 +1,63 @@
+const MongoClient = require('mongodb').MongoClient;
+const url = require('url');
+
 const express = require('express');
+const router=express.Router();
+
 const path = require('path');
+
 const generatePassword = require('password-generator');
+const generateEmail = require('random-email');
 
 const app = express();
 
+
+
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
+
+
+
+
+
+router.get('/',function(req,res,next){ 
+  MongoClient.connect(process.env.MONGODB_URI,function(err,db){
+    if(err) throw err;
+    var dbo=db.db('heroku_tj9btv1k');
+
+    dbo.createCollection("id_table",function(err,res){
+      if(err) throw err;
+      db.close();
+    });
+    dbo.createCollection("status_table",function(err,res){
+      if(err) throw err;
+      db.close();
+    });
+
+
+
+    dbo.collection('UserInput').insertOne({
+      item: "canvas", qty: 100, tags: ["cotton"], size: { h: 28, w: 35.5, uom: "cm" }
+    }, function(err,res){
+      if(err) throw err;
+      console.log("# of docs inserted: " + res.insertedCount);
+      db.close();
+    });
+
+
+  });
+});
+
+
+
+
+
+
+
+
+
+
+
 
 // Put all API endpoints under '/api'
 app.get('/api/passwords', (req, res) => {
@@ -19,8 +71,34 @@ app.get('/api/passwords', (req, res) => {
   // Return them as json
   res.json(passwords);
 
-  console.log(`Sent ${count} passwords`);
+  console.log(`Sent ${passwords} passwords`);
 });
+
+
+
+
+app.get('/api/emails',(req,res) => {
+
+  
+  //var email = generateEmail({domain: 'gmail.com'});
+  const email2 = Array.from(Array(5).keys()).map(i =>
+    generateEmail({domain: 'gmail.com'})
+  )
+
+  res.json(email2);
+  console.log(`${email2}`);
+  
+  
+});
+
+
+
+
+
+
+
+
+
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
@@ -28,7 +106,18 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
 
+
+
+
 const port = process.env.PORT || 5000;
 app.listen(port);
 
 console.log(`Password generator listening on ${port}`);
+
+
+
+
+
+
+
+
